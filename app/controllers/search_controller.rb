17585c31c1341783eadeacad6c06a3211d87bc93
jason <jason@coverhound.com>
2016-02-27 19:45:57 -0800

    moving things to the other controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,31 @@
 class SearchController < ApplicationController
+
+
+
+  def expedia_query(location, startdate, enddate)
+
+    expedia_key = 'wRhEfj9STYhqVcbFd6yuTyBF2GukBVtD'
+    expedia_secret = 'yRpakAdxHIRWjm3D'
+
+    response = HTTParty.get('http://terminal2.expedia.com/x/activities/search?location=' + location + '&startDate=' + startdate + '&endDate=' + enddate + '&apikey=' + expedia_key)
+
+    response.body
+
+  end
+
+  def build_result_array(api_response)
+
+
+
+  end
+
+
+
+
+
+
+
+
+
+
 end
