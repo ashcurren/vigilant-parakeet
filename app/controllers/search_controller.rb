5adc831d0b74244358b64c601ee38e9689eb605c
jason <jason@coverhound.com>
2016-02-27 19:54:10 -0800

    moving things

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
 
+  def index
 
+  end
+
+  def run_search
+
+  end
 
   def expedia_query(location, startdate, enddate)
 
@@ -13,19 +19,18 @@ class SearchController < ApplicationController
 
   end
 
-  def build_result_array(api_response)
+  def uber_query(sLat, sLong, eLat, eLong)
 
+    sLat = sLat.to_s
+    sLong = sLong.to_s
+    eLat = eLat.to_s
+    eLong = eLong.to_s
 
+    response = HTTParty.get('https://api.uber.com/v1/estimates/price?start_latitude='+sLat+'&start_longitude='+sLong+'&end_latitude='+eLat+'&end_longitude='+eLong, headers: {"Authorization" => "Token OOWRpDScLJvhdQm4yUfmA24r_Kz0OKNdoKh7MuC1
+s7AlWnIrepGsLoi6db-Oct6DCYrhatz9pmRQ-bOu"})
 
+    response.body
   end
-
-
-
-
-
-
-
-
 
 
 end
